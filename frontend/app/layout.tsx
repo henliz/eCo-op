ace5304229efd8d7231e5918bb05fc8a7a56e0f6
andrevanniekerk <andre@guidemyai.com>
2025-05-19 19:04:02 -0400

    tracking!

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from '@vercel/analytics/react';
 
 import "./globals.css";
 
@@ -57,6 +58,9 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <Analytics
+              mode={process.env.NODE_ENV === 'production' ? 'production' : 'development'}
+        />
       </body>
     </html>
   );
